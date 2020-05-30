527da269fc05a8a5444ed71aedc5f7e82a6ee980
Esmeralda Hdez <esmeyhg@gmail.com>
2020-05-29 19:38:40 -0500

    Proyecto

---
+++ NutriSoft/app/src/main/java/com/example/nutrisoft/ErrorCreateAcc.java
@@ -0,0 +1,14 @@
+package com.example.nutrisoft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ErrorCreateAcc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error_create_acc);
+    }
+}
---
+++ NutriSoft/app/src/main/java/com/example/nutrisoft/VisorComida.java
@@ -0,0 +1,14 @@
+package com.example.nutrisoft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisorComida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visor_comida);
+    }
+}
